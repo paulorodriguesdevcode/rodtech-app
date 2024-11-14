0eb231b74594a939167c78b3d1da5b5c1ff0cb44
Paulo Rodrigues <P39210@parceirosedu.com.br>
2024-11-14 14:03:34 -0300

    fix: cards

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -7,15 +7,20 @@ import { exportOrdersToExcel } from "../services/export-orders-excel";
 import { toast } from "react-toastify";
 import Button from "../components/common/Button";
 import { Order } from "../types/order";
+import Card from "../components/common/Card";
 
 export default function Page() {
   const [orders, setOrders] = useState<Order[]>([])
   const [isLoading, setIsLoading] = useState(true);
+  const [totals, setTotals] = useState(
+    { totalQuantity: 0, totalValue: 0 }
+  );
 
   const fetchOrders = useCallback(async () => {
     try {
       const ordersFromDb = await listOrders();
       setOrders(ordersFromDb);
+      setTotals(calculateTotals(ordersFromDb));
     } catch (error: unknown) {
       if (error instanceof Error) {
         toast.error(error.message);
@@ -31,12 +36,27 @@ export default function Page() {
     fetchOrders();
   }, [fetchOrders]);
 
+  const calculateTotals = (orders: Order[]) => {
+    return orders?.reduce(
+      (acc, order) => {
+        acc.totalQuantity += order.quantity;
+        acc.totalValue += (order.totalValue || 0);
+        return acc;
+      },
+      { totalQuantity: 0, totalValue: 0 }
+    );
+  };
+
   return (
     <div>
       <div className="p-4 xl:ml-80 ">
         <div className="mt-20">
           <div className="mb-12 grid">
             <Button text='Exportar em excel' classType={"CONFIRM"} onClick={() => exportOrdersToExcel(orders)} specialClass="h-20" />
+          </div>
+          <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-2">
+            <Card isLoading={isLoading} title="Quantidade de produtos" value={(totals.totalQuantity.toString()).toString()} />
+            <Card isLoading={isLoading} title="Total em R$ vendido" value={totals.totalValue.toString() } />
           </div>
           <div className='flex-wrap'>
             <OrdersTable orders={orders} isLoading={isLoading} updateOrders={() => fetchOrders()} />
